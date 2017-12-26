aad05b66aa1921c4a86a12d4ca98533751021ef3
Anael Dahan <anael.assayag@gmail.com>
2017-12-26 11:31:44 +0200

    add an option to rate the beer and display the rate with the list of the beers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+debugger
 
-function addBeer(name, category) {
-    beers.push({name:name , category:category});
+function addBeer(name, category, rate) {
+    beers.push({name:name , category:category, rate:rate});
    }
 
 var beers = [];
@@ -8,19 +9,22 @@ var beers = [];
 
 function renderBeers() {
     $('.beers-list').find('li').remove();
+    //$('.beers-list').find('option').remove();
 
     for(var i=0; i<beers.length; i++) {
-        $('.beers-list').append('<li>'+ beers[i].name + (' ') + beers[i].category + '</li>');
+        $('.beers-list').append('<li>'+ beers[i].name + (' ') + beers[i].category + (' ')+ beers[i].rate + '</li> ' );
+        //$('.beers-list').append( '<option>' + beers[i].rate  + (' stars') + '</option> ')
 
     }
-}
 
+}
+ 
 
 var clicked = function (beerName, beerCategory) {
     var beerName = $('.beer-input').val();
     var beerCategory = $('.category-input').val(); 
-    var noteBeer = $('.note').val()
-    addBeer(beerName, beerCategory)
+    var beerRate = $('.rate-select').val();
+    addBeer(beerName, beerCategory, beerRate)
     renderBeers()
 }
 $('button').on('click', clicked);
