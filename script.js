8bb7e12c950f9b332eb30cfb62f36b59d1d32f11
Anael Dahan <anael.assayag@gmail.com>
2017-12-26 15:43:43 +0200

    add buttton to sort beer according to their notation

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
 debugger
+var beers = [];
+var order = true
+
 
 function addBeer(name, category, rate) {
-    beers.push({name:name , category:category, rate:rate});
-   }
+    beers.push({ name: name, category: category, rate: rate });
+}
 
-var beers = [];
 
 
 function renderBeers() {
     $('.beers-list').find('li').remove();
-    //$('.beers-list').find('option').remove();
 
-    for(var i=0; i<beers.length; i++) {
-        $('.beers-list').append('<li>'+ beers[i].name + (' ') + beers[i].category + (' ')+ beers[i].rate + '</li> ' );
-        //$('.beers-list').append( '<option>' + beers[i].rate  + (' stars') + '</option> ')
+
+    for (var i = 0; i < beers.length; i++) {
+        $('.beers-list').append('<li>' + beers[i].name + (' ') + beers[i].category + (' ') + beers[i].rate + ' stars' + '</li> ');
 
     }
 
 }
- 
 
-var clicked = function (beerName, beerCategory) {
+//button to display beers and their notation;
+
+var addNwBeer = function (beerName, beerCategory) {
     var beerName = $('.beer-input').val();
-    var beerCategory = $('.category-input').val(); 
+    var beerCategory = $('.category-input').val();
     var beerRate = $('.rate-select').val();
     addBeer(beerName, beerCategory, beerRate)
     renderBeers()
+    $('.form-group').find('form-control').remove();
+
+
 }
-$('button').on('click', clicked);
+$('#btn1').on('click', addNwBeer);
+
+//button to sort beers;
+var clickedRate = function () {
+    if (order) {
+        beers.sort(function (a, b) {
+            return a.rate - b.rate;
+        })
+        order = false
+    }
+    if (order == false) {
+        beers.sort(function (a, b) {
+            return a.rate + b.rate;
+        })
+        order == true
+    }
+    renderBeers()
+}
+
+$('#btn2').on('click', clickedRate);
+
+
+
+
+
 
 
